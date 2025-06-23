f565d6fbeb41c4dc13fb91e197e75ceef2e0fda3
FJDubs <taggartf1@gmail.com>
2025-06-23 18:31:51 -0400

    Implemented cleanInput to take a string of words and return a slice of strings. Added unit testing

---
+++ repl_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+		{
+			input:    "charmander squirtle bulbasaur",
+			expected: []string{"charmander", "squirtle", "bulbasaur"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("case: %s is not the same length as expected", c.input)
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("case: %s does not match expected: %s", word, expectedWord)
+			}
+		}
+	}
+
+}
